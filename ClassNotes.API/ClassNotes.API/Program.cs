4a4b781f84c421df27ba727b83af4beb50a6f498
TETvega <hectormartinez1vg@gmail.com>
2025-03-02 00:43:26 -0600

    Chore [ Program - Template] : Actualizacion
    - Se Actualizo el Tamplate
    - Se Descomento el Program
    - Se Probo Claudinary

--- ClassNotes.API/ClassNotes.API/Program.cs
+++ ClassNotes.API/ClassNotes.API/Program.cs
@@ -14,24 +14,24 @@ var app = builder.Build();
 startup.Configure(app, app.Environment);
 
 // AM: using para cargar la data del seeder
-//using (var scope = app.Services.CreateScope())
-//{
-//	var services = scope.ServiceProvider;
-//	var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+using (var scope = app.Services.CreateScope())
+{
+	var services = scope.ServiceProvider;
+	var loggerFactory = services.GetRequiredService<ILoggerFactory>();
 
-//	try
-//	{
-//		var context = services.GetRequiredService<ClassNotesContext>();
-//		var userManager = services.GetRequiredService<UserManager<UserEntity>>();
-//		var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+	try
+	{
+		var context = services.GetRequiredService<ClassNotesContext>();
+		var userManager = services.GetRequiredService<UserManager<UserEntity>>();
+		var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
 
-//		await ClassNotesSeeder.LoadDataAsync(context, loggerFactory, userManager, roleManager);
-//	}
-//	catch (Exception e)
-//	{
-//		var logger = loggerFactory.CreateLogger<Program>();
-//		logger.LogError(e, "Error al ejecutar el Seed de datos");
-//	}
-//}
+		await ClassNotesSeeder.LoadDataAsync(context, loggerFactory, userManager, roleManager);
+	}
+	catch (Exception e)
+	{
+		var logger = loggerFactory.CreateLogger<Program>();
+		logger.LogError(e, "Error al ejecutar el Seed de datos");
+	}
+}
 
 app.Run();
