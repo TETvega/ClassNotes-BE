995c1be708fdc6b085ff80d60f953dd9c524e14c
unknown <kengalguevara@gmail.com>
2025-03-02 12:52:10 -0600

    feat(api): Creación de la entidad 'StudentUnitEntity'
    Se agregó la entidad 'StudentUnitEntity', que actúa como una relación entre 'StudentEntity' y 'UnitEntity', permitiendo almacenar y acceder a la calificación de un estudiante en una unidad específica.

---
+++ ClassNotes.API/ClassNotes.API/Database/Entities/StudentUnitEntity.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ClassNotes.API.Database.Entities
+{
+    [Table("students_units", Schema = "dbo")]
+    public class StudentUnitEntity : BaseEntity
+    {
+        [Required]
+        [Column("unit_id")]
+        public Guid UnitId { get; set; }
+        [ForeignKey(nameof(UnitId))]
+        public virtual UnitEntity Unit { get; set; }
+
+
+        [Required]
+        [Column("student_id")]
+        public Guid StudentId { get; set; }
+        [ForeignKey(nameof(StudentId))]
+        public virtual StudentEntity Student { get; set; }
+
+
+        [Required]
+        [Column("unit_note")]
+        [Range(0,100)]
+        public float UnitNote { get; set; }
+
+
+
+        public virtual UserEntity CreatedByUser { get; set; }
+        public virtual UserEntity UpdatedByUser { get; set; }
+    }
+}
