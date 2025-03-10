66d3a4b837548f0ff93a0952d74afe173d98fe2b
Pineda04 <carlosovidiopineda8@gmail.com>
2025-03-10 15:04:58 -0600

    feat [COURSES] : Agregar dashboard de curso
    - Se implementó el dashboard del curso, accesible solo para el docente creador.
    - Muestra información relevante del curso:
      - Cantidad de estudiantes inscritos.
      - Número de actividades pendientes.
      - Total de puntos evaluados.
      - Lista de estudiantes del curso.
      - Lista de actividades del curso.
    - Se añadieron validaciones para restringir el acceso solo al docente creador.
    
    Closed #89

---
+++ ClassNotes.API/ClassNotes.API/Controllers/DashboardCoursesController.cs
@@ -0,0 +1,31 @@
+﻿using ClassNotes.API.Constants;
+using ClassNotes.API.Dtos.Common;
+using ClassNotes.API.Dtos.DashboardCourses;
+using ClassNotes.API.Services.DashboardCourses;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClassNotes.API.Controllers
+{
+    // --------------------- CP --------------------- //
+    [ApiController]
+    [Route("api/dashboard_courses")]
+    [Authorize(AuthenticationSchemes = "Bearer")]
+    public class DashboardCoursesController : ControllerBase
+    {
+        private readonly IDashboardCoursesService _dashboardCoursesService;
+
+        public DashboardCoursesController(IDashboardCoursesService dashboardCoursesService)
+        {
+            _dashboardCoursesService = dashboardCoursesService;
+        }
+
+        [HttpGet("{courseId}/info")] // En el bruno se debe de poner el id del curso y luego el /info
+        [Authorize(Roles = $"{RolesConstant.USER}")]
+        public async Task<ActionResult<ResponseDto<DashboardCourseDto>>> GetDashboardInfo(Guid courseId)
+        {
+            var result = await _dashboardCoursesService.GetDashboardCourseAsync(courseId);
+            return StatusCode(result.StatusCode, result);
+        }
+    }
+}
