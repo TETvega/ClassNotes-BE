ddbfefc2c92f24e72de350cf2078db7c7b2a73b2
TETvega <hectormartinez1vg@gmail.com>
2025-04-18 01:32:26 -0600

    Fix [Number DAY of The Month]: Student Attendances
    - Retorno del numero de dia (string)

--- ClassNotes.API/ClassNotes.API/Dtos/Attendances/Student/ExtendedDateDto.cs
+++ ClassNotes.API/ClassNotes.API/Dtos/Attendances/Student/ExtendedDateDto.cs
@@ -5,6 +5,7 @@
         public string Minute { get; set; }
         public string Hour { get; set; }
         public string Day { get; set; }
+        public string NumberDay { get; set; }
         public string Month { get; set; }
         public string Year { get; set; }
     }
