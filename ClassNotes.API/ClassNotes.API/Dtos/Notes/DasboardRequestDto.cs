777ece5c75b1a6fd7bca3c70bb236533a0fc0b2f
unknown <kengalguevara@gmail.com>
2025-04-18 22:09:49 -0600

    fix[students-activities]: corrección en manejo de datos en varias clases
    - Se modificó el dto 'DasboardRequestDto' para incluir la fecha de finalización del curso.
    - Se modificó el endpoint 'GetAllStudentNotesUnits' para incluir esta propiedad en el dto que retorna.

--- ClassNotes.API/ClassNotes.API/Dtos/Notes/DasboardRequestDto.cs
+++ ClassNotes.API/ClassNotes.API/Dtos/Notes/DasboardRequestDto.cs
@@ -4,8 +4,10 @@ using ClassNotes.API.Dtos.Notes.QualificationDasboard;
 namespace ClassNotes.API.Dtos.Notes
 {
     public class DasboardRequestDto
-    {
+    { 
+        public DateTime? EndDate { get; set; }
         public StadisticStudentsDto StadisticStudents { get; set; }
+       
 
         public PaginationDto<List<StudentQualificationDto>> StudentQualifications { get; set; }
     }
